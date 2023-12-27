ffdbc1f05f2861161ce81525e82cfb845a581667
Dabai-Sun <907928246@qq.com>
2023-12-28 00:15:47 +0800

    项目初始化-调整初始目录结构

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <!-- 路由的出口 -->
+    <router-view />
+    <p>123</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="less">
+</style>
